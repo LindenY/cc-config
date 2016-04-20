9fa0cdd73ef1ef895ca1305f0376e81fe3fb3fea
DesLin <desmond.zq.lin@gmail.com>
2016-04-20 00:20:27 -0400

    add assign and remove button for each agents under dids

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -1,43 +1,70 @@
 'use strict';
 
-angular
-    .module('myApp.view1', ['ngRoute'])
+var didConfigModule = angular.module('myApp.view1', ['ngRoute'])
     .config(['$routeProvider', function($routeProvider) {
         $routeProvider.when('/view1', {
         templateUrl: 'view1/view1.html',
         controller: 'View1Ctrl'
     });
-}])
+}]);
 
-.controller('View1Ctrl', ['$scope', function($scope) {
+
+var didsPanelCtrl = didConfigModule.controller('View1Ctrl', ['$scope', function($scope) {
     $scope.dids = [
         {
             name: "didA",
             local: "localA",
             assigning: false,
-            agents: [{name: 'agent1'}, {name: 'agent2'}],
-            unassigned: [
-                {
-                    name: "agent3",
-                    selected: false
-                }
-            ]
+            assignedAgents: {
+                agent1: { name: 'agent1' },
+                agent3: { name: 'agent3' },
+            },
+            unassignedAgents: {
+                agent2: { name: 'agent2' },
+            }
         },
         {
             name: 'didB',
             local: 'localB',
             assigning: false,
-            agents: [{name: 'agent2'}],
-            unassigned: [
-                            {
-                                name: "agent1",
-                                selected: false
-                            },
-                            {
-                                name: "agent3",
-                                selected: false
-                            }
-                        ]
+            assignedAgents: {
+                agent1: { name: 'agent1' },
+            },
+            unassignedAgents: {
+                agent2: { name: 'agent2' },
+                agent3: { name: 'agent3' },
+            }
         }
     ];
+}]);
+
+var singleDidConfigCtrl = didConfigModule.controller('SingleDidConfigCtrl', ['$scope', function($scope) {
+
+    // controller init function
+    $scope.init = function(didInfo) {
+        $scope.didInfo = didInfo;
+    }
+
+    $scope.getAgentsWithAssignedState = function(ifAssigned) {
+        var result = [];
+        for (var key in $scope.didInfo.agents) {
+            if ($scope.didInfo.agents[key].assigned == ifAssigned) {
+                result.push(key);
+            }
+        }
+        return result;
+    }
+
+    // assign agent to did
+    $scope.assignAgent = function(agent) {
+        $scope.didInfo.assignedAgents[agent.name] = agent;
+        delete $scope.didInfo.unassignedAgents[agent.name];
+    }
+
+    // remove agent from did
+    $scope.removeAgent = function(agent) {
+        $scope.didInfo.unassignedAgents[agent.name] = agent;
+        delete $scope.didInfo.assignedAgents[agent.name]
+    }
+
 }]);
